918c98af7731bffbae532ecab6102a41ae23c4ea
Otsuka Kohei <otsuka_kunikura@outlook.com>
2018-02-24 17:30:56 +0900

    設定用のFragmentを追加

---
+++ app/src/main/java/com/otsuka/simplemio/fragments/ConfigFragment.kt
@@ -0,0 +1,19 @@
+package com.otsuka.simplemio.fragments
+
+import android.os.Bundle
+import android.preference.PreferenceFragment
+import com.otsuka.simplemio.R
+
+
+/**
+ * Created by otsuka on 2018/02/24.
+ */
+class ConfigFragment : PreferenceFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // /app/res/xml/preference.xml に定義されている設定画面を適用
+        addPreferencesFromResource(R.xml.preference)
+    }
+}
